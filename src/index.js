64c4f8d61249889b6bb9288c2372626915622165
barbsuk20 <146033546+barbsuk20@users.noreply.github.com>
2023-12-25 20:46:41 +0000

    added the current location

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ function losAngeles() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector(".city-container");
